fb3b19d3d19b039249bf06c5fbc17bc282cda88a
symbios-zi <symbios@rootnix.net>
2022-03-27 14:04:45 +0300

    added anotation to helper dd

--- App/Support/helpers.php
+++ App/Support/helpers.php
@@ -1,10 +1,14 @@
 <?php
 
-
-function dd($data)
+/**
+ * Функция помогает посмотреть данные в любой переменной.
+ * @param $variable
+ *
+ */
+function dd($variable)
 {
     echo "<pre>";
-    var_dump($data);
+    var_dump($variable);
     echo "</pre>";
     die;
 }
